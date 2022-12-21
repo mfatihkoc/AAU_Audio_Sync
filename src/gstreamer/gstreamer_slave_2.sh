d27a3a0c66a9cb8b603f70b8748b01235b7c8302
Magnus <magnus@bogh.dk>
2022-12-21 17:45:45 +0000

    added second gstreamer slave, changed second gstreamer ip address, added alsa conf

---
+++ src/gstreamer/gstreamer_slave_2.sh
@@ -0,0 +1,22 @@
+gst-launch-1.0 -v udpsrc port=12345 ! application/x-rtp,media=audio,clock-rate=48000, channels=1,encoding-name=L16 ! rtpjitterbuffer mode=4  name=jitterBuffer ! rtpL16depay ! audioconvert  ! alsasink  sync=true
+
+#Launch Gstreamer with verbose output
+#gst-launch-1.0 -v 
+
+#Reads udp packets on network
+#udpsrc port=12345
+
+#??
+#application/x-rtp,media=audio,clock-rate=48000, channels=1,encoding-name=L16
+
+#??
+#rtpjitterbuffer mode=4  name=jitterBuffer
+
+#??
+#rtpL16depay
+
+#Tells gstreamer to start audio conversion
+#audioconvert
+
+#??
+#alsasink  sync=true
